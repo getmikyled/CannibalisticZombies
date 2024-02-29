d6f889877dbeb7dd708ef216e79f282dbf68820e
radnonymous <74389363+radnonymous@users.noreply.github.com>
2024-02-28 16:42:59 -0800

    Animated Health UI

--- CannibalisticZombies/Assets/Scripts/HealthUI.cs
+++ CannibalisticZombies/Assets/Scripts/HealthUI.cs
@@ -8,58 +8,67 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
     // HealthUI displays current health.
-    // Calling UpdateHealth() with new Damage amount with update healthBar to that amount.
-    public GameObject health;
+    // Calling SetHealth() with new Damage amount update healthBar to that amount.
     public Image healthBar;
-    public float currentHealth;
+    public float currentHealth;//unnecessary in implementation
+    public float animationSpeed; //Rate at which damage bar fills/empties per frame
+    public Color damageColor=Color.red;//Color of bar when take damage
+    public Color healColor=Color.green; //Color of bar when heals/removes damage
+    private float fillTarget;//Decimal amount of damage bar to be filled
 
-    public  float animationTime=1f; // The time it takes the damge bar to move up/down
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
         TestMethod(); // Remove in actual implementation
+        UpdateHealth();
     }
 
-    public void UpdateHealth(float newHealth, float maxHealth = 100f)
+    public void SetHealth(float newHealth, float maxHealth = 100f)
     {
         //newHealth is the newHealth to be displayed
         //maxHealth is maximum health, defaults to 100
-        float fillTarget = 1- newHealth / maxHealth;
-        //float fillDiff = fillTarget - healthBar.fillAmount;
+        fillTarget = 1- newHealth / maxHealth;
         fillTarget = Mathf.Clamp(fillTarget, 0, 1);
-        float time = Time.time;
-        if (newHealth > currentHealth){
-            healthBar.color = Color.green;
-        }
-        else
-        {
-            healthBar.color = Color.red;
-        }
-        Debug.Log(fillTarget);
-        healthBar.fillAmount= fillTarget;
-        Debug.Log(healthBar.fillAmount);
         currentHealth = Mathf.Clamp(newHealth,0,maxHealth);
+        Debug.Log(fillTarget);
+        Debug.Log(healthBar.fillAmount);
+
     }
 
     private void TestMethod()
     {
-        // Only called in Update() for testing, not needed for actual implementation
+        // Called in Update() for testing, not needed for actual implementation
+        //Press enter to take 20 damage
+        //Press space to heal 10 damage
         if (Input.GetKeyUp(KeyCode.Return))
         {
-            UpdateHealth(currentHealth - 20);
+            SetHealth(currentHealth - 20);
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
-            UpdateHealth(currentHealth + 10);
+            SetHealth(currentHealth + 10);
         }
+    }
+
+    private void UpdateHealth() {
+        //Call in Update function to animate HealthBar
+        if(healthBar.fillAmount == fillTarget) { return; }//When health bar is up to date
+        if (Mathf.Abs(healthBar.fillAmount - fillTarget) <= animationSpeed)//When frame animation is negligible
+        { 
+            healthBar.color = damageColor;
+            healthBar.fillAmount = fillTarget;
+        }
+        if (healthBar.fillAmount< fillTarget) {//When player takes damage
+            healthBar.fillAmount += animationSpeed;
+        }
+        if(healthBar.fillAmount > fillTarget)//When player heals/removes damage
+        {
+            healthBar.color = healColor;
+            healthBar.fillAmount -=animationSpeed;
+        }
+
     }
 
 }
