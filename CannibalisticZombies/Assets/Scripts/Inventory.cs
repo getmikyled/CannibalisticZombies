b8b605ae6ac423610a2c01ee0c44f5248d351d90
radnonymous <74389363+radnonymous@users.noreply.github.com>
2024-03-08 11:22:23 -0800

    Implement Methods
    Create Methods for Inventory and InventorySlots

--- CannibalisticZombies/Assets/Scripts/Inventory.cs
+++ CannibalisticZombies/Assets/Scripts/Inventory.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Inventory 
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<InventorySlot> inventory;
+
+
+    public Inventory() 
     {
-        
+        inventory = new List<InventorySlot>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //-/////////////////////////////////////////////////////////////////////
+    /// adds amount of new item to existing InventorySlot with said item 
+    public void AddItem(Item newItem, int amount = 1)
     {
-        
+        foreach (InventorySlot slot in inventory) 
+        {
+            if (slot.GetItem() == newItem) 
+            {
+                slot.AddItem(amount);
+                return;
+            }
+        }
+        inventory.Add(new InventorySlot(newItem, amount));
+    }
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// returns weight of all inventory slots
+    public int GetCurrentWeight() 
+    {
+        int weight = 0;
+        foreach (InventorySlot slot in inventory) 
+        {
+            weight += slot.GetWeight();
+        }
+        return weight;
     }
 }
--- CannibalisticZombies/Assets/Scripts/InventorySlot.cs
+++ CannibalisticZombies/Assets/Scripts/InventorySlot.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
-public class InventorySlot : MonoBehaviour
+
+public class Item // Placeholder until Item class implemented
 {
-    // Start is called before the first frame update
-    void Start()
+    public int weight;
+} 
+public class InventorySlot
+{
+
+    private Item item; 
+    private int amount; // Amount of the item stored
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// Constructor
+    public InventorySlot(Item newItem, int startAmount = 1)
     {
-        
+        item = newItem;
+        amount = startAmount;
     }
 
-    // Update is called once per frame
-    void Update()
+    //-/////////////////////////////////////////////////////////////////////
+    /// return item
+    public Item GetItem()
     {
-        
+        return item;
+    }
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// return amount
+    public int GetAmount() 
+    { 
+        return amount;
+    }
+
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// adds amount of item to the slot, default add 1
+    public void AddItem(int addedAmount = 1)
+    {
+        amount += addedAmount;
+    }
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// removes amount of item, default remove 1
+    /// returns true if successfully removed removeAmount 
+    /// returns false if not enough available or input is nonpositive int <summary>
+    /// resets slot if no items stored
+    public bool RemoveItem(int removedAmount = 1)
+    {
+        if (removedAmount < 0)// 
+        { 
+            //Debug.Log("Invalid item amount request: " + removedAmount);
+            return false;
+        }
+        if (amount <=removedAmount) 
+        {
+            amount -= removedAmount;
+            if (amount == 0) { Reset(); }
+            return true;
+        }
+        //Debug.Log("Less than " + removedAmount + item + " available (" + amount + ")");
+        return false;
+    }
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// Returns weight of slot based on item weight
+    public int GetWeight()
+    {
+        return amount * item.weight;
+    }
+
+    //-/////////////////////////////////////////////////////////////////////
+    /// Resets slot to default values
+    public void Reset() 
+    {
+        item = null;
+        amount = 0;
     }
 }
